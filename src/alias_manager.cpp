f82171fe17d748d373b204a5116d01034b3becf1
BenjaminSchwonburg <benjamin.schwonburg@proton.me>
2026-02-12 00:05:59 +0100

    fix: psal now only uses one universal aliases.json

--- src/alias_manager.cpp
+++ src/alias_manager.cpp
@@ -1,26 +1,43 @@
 #include "json.hpp"
 #include "alias_manager.hpp"
 #include <fstream>
+#include <windows.h>
+#include <string>
 
 using json = nlohmann::json;
 
-void loadAliases(AliasMap& aliases, const std::string& filename) {
-    std::ifstream file(filename);
+std::string getExecutableDir();
+
+void saveAliases(const std::unordered_map<std::string, Alias>& aliases) {
+    std::string aliasFile = getExecutableDir() + "\\aliases.json";
+    json j;
+    for (const auto& [name, a] : aliases) {
+        j[name] = { {"command", a.command}, {"description", a.description} };
+    }
+    std::ofstream file(aliasFile);
+    file << j.dump(4);
+}
+
+void loadAliases(std::unordered_map<std::string, Alias>& aliases) {
+    std::string aliasFile = getExecutableDir() + "\\aliases.json";
+    std::ifstream file(aliasFile);
     if (!file.is_open()) return;
 
     json j;
     file >> j;
 
     for (auto& [name, value] : j.items()) {
-        aliases[name] = { value["command"].get<std::string>(), value.value("description","") };
+        aliases[name] = {
+            value["command"].get<std::string>(),
+            value.value("description", "")
+        };
     }
 }
 
-void saveAliases(const AliasMap& aliases, const std::string& filename) {
-    json j;
-    for (const auto& [name, a] : aliases) {
-        j[name] = { {"command", a.command}, {"description", a.description} };
-    }
-    std::ofstream file(filename);
-    file << j.dump(4);
+std::string getExecutableDir() {
+    char path[MAX_PATH];
+    GetModuleFileNameA(NULL, path, MAX_PATH);
+    std::string fullPath(path);
+    auto pos = fullPath.find_last_of("\\/");
+    return fullPath.substr(0, pos);
 }
